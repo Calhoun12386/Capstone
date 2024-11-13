ba6c8c6f9cf065807bed1ea24b40fc543a4a3c83
Shamima Sultana <shamimasultana@Shamimas-Laptop.lan>
2024-11-12 21:38:29 -0500

    update env vars

--- src/API/api.js
+++ src/API/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://localhost:3000/api'; 
+const API_BASE_URL = process.env.API_URL || 'http://localhost:3000/api';
 
 // Function to fetch all books
 export const fetchBooks = async () => {
